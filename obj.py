7af8a85e0ca32f4a639c4066fbfe8be51817f8a1
Jake Ross <jakeross@jakes-mbp.attlocal.net>
2017-09-25 15:42:19 -0500

    update

--- obj.py
+++ obj.py
@@ -2,18 +2,36 @@
 # as a convention, always capitalize the first letter of the object
 class Dog(object):
 
-    #class object attribute, outside of the function
+    #class object attribute, outside of the function, 'global scope' if you will
     species = 'mammal'
+    location = 'Texas'
 
-    def __init__(self, breed, name):
+    def __init__(self, breed, name, fur, fixed, age):
 
         self.breed = breed
         self.name = name
+        self.fur = fur
+        self.fixed = fixed
+        self.age = age
 
-sam = Dog(breed = 'Lab', name = 'Joey')
+sam = Dog(breed = 'Lab',
+          name = 'Joey',
+          fur = True,
+          fixed = False,
+          age = 4)
 
 print sam.name
 print sam.breed
+print sam.age
+print sam.fixed
+print sam.fur
+print sam.location
 
-# Joey
-# Lab
+'''
+Joey
+Lab
+4
+False
+True
+Texas
+'''
